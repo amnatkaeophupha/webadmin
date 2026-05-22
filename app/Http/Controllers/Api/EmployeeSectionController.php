bee2a787fd1d90f642760f0fd5f9937179297e73
amnatkaeophupha <phuphakaeo@gmail.com>
2026-05-22 17:05:25 +0700

    update employee section seeder with auto slug and section_type

--- app/Http/Controllers/Api/EmployeeSectionController.php
+++ app/Http/Controllers/Api/EmployeeSectionController.php
@@ -4,13 +4,35 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\EmployeeSection;
+use Illuminate\Http\Request;
+
 class EmployeeSectionController extends Controller
 {
-    public function index()
+
+    // public function index()
+    // {
+    //     return EmployeeSection::with(['website', 'children'])
+    //         ->whereNull('parent_id')
+    //         ->orderBy('sort_order')
+    //         ->get();
+    // }    
+    
+    public function index(Request $request)
     {
-        return EmployeeSection::with(['website', 'children'])
+        $websiteId = $request->get('website_id');
+
+        $sections = EmployeeSection::with('children.children') // รองรับ 3 ชั้น
+            ->where('website_id', $websiteId)
             ->whereNull('parent_id')
+            ->where('is_active', 1)
             ->orderBy('sort_order')
             ->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $sections
+        ]);
     }
+
+
 }
